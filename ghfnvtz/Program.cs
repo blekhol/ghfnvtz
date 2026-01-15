a2806273ab0a8e239d779dc1018a3793c7ee032d
Petrik Diak <diak@petrik.hu>
2026-01-15 08:51:05 +0100

    base

--- ghfnvtz/Program.cs
+++ ghfnvtz/Program.cs
@@ -7,10 +7,12 @@
             Window window = new Window();
             window.Setup();
 
-            ConsoleKeyInfo key;
+			string mode = "";
+
+			ConsoleKeyInfo key;
             while (true)
             {
-                key = Console.ReadKey();
+                key = Console.ReadKey(true);
 
                 switch (key.Key)
                 {
@@ -54,8 +56,121 @@
 							break;
 						}
 						break;
-                    
-                    default:
+                    case ConsoleKey.Spacebar:
+						if (mode == "color")
+						{
+							mode = "";
+						}
+						else
+						{
+							mode = "color";
+						}
+						break;
+					case ConsoleKey.P:
+						if (mode == "color")
+						{
+							if (key.Modifiers == ConsoleModifiers.Shift)
+							{
+								window.DrawAtPos('P');
+							}
+							else
+							{
+								window.DrawAtPos('p');
+							}
+						}
+						break;
+					case ConsoleKey.F:
+						if (mode == "color")
+						{
+							if (key.Modifiers == ConsoleModifiers.Shift)
+							{
+								window.DrawAtPos('F');
+							}
+							else
+							{
+								window.DrawAtPos('f');
+							}
+						}
+						break;
+					case ConsoleKey.K:
+						if (mode == "color")
+						{
+							if (key.Modifiers == ConsoleModifiers.Shift)
+							{
+								window.DrawAtPos('K');
+							}
+							else
+							{
+								window.DrawAtPos('k');
+							}
+						}
+						break;
+					case ConsoleKey.Z:
+						if (mode == "color")
+						{
+							if (key.Modifiers == ConsoleModifiers.Shift)
+							{
+								window.DrawAtPos('Z');
+							}
+							else
+							{
+								window.DrawAtPos('z');
+							}
+						}
+						break;
+					case ConsoleKey.C:
+						if (mode == "color")
+						{
+							if (key.Modifiers == ConsoleModifiers.Shift)
+							{
+								window.DrawAtPos('C');
+							}
+							else
+							{
+								window.DrawAtPos('c');
+							}
+						}
+						break;
+					case ConsoleKey.M:
+						if (mode == "color")
+						{
+							if (key.Modifiers == ConsoleModifiers.Shift)
+							{
+								window.DrawAtPos('M');
+							}
+							else
+							{
+								window.DrawAtPos('m');
+							}
+						}
+						break;
+					case ConsoleKey.S:
+						if (mode == "color")
+						{
+							if (key.Modifiers == ConsoleModifiers.Shift)
+							{
+								window.DrawAtPos('S');
+							}
+							else
+							{
+								window.DrawAtPos('s');
+							}
+						}
+						break;
+					case ConsoleKey.X:
+						if (mode == "color")
+						{
+							if (key.Modifiers == ConsoleModifiers.Shift)
+							{
+								window.DrawAtPos('X');
+							}
+							else
+							{
+								window.DrawAtPos('x');
+							}
+						}
+						break;
+					default:
                         break;
                 }
             }
--- ghfnvtz/Window.cs
+++ ghfnvtz/Window.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ghfnvtz
 {
@@ -11,18 +12,56 @@ namespace ghfnvtz
         private int width;
         private int height;
 
+        private Dictionary<char, ConsoleColor> colors = new Dictionary<char, ConsoleColor>()
+        {
+            {'f' , ConsoleColor.Black },
+            { 'F' , ConsoleColor.White },
+            { 'k' , ConsoleColor.Blue },
+			{ 'K' , ConsoleColor.DarkBlue },
+			{ 'z' , ConsoleColor.Green },
+			{ 'Z' , ConsoleColor.DarkGreen },
+			{ 'c' , ConsoleColor.Cyan },
+			{ 'C' , ConsoleColor.DarkCyan },
+			{ 'p' , ConsoleColor.Red },
+			{ 'P' , ConsoleColor.DarkRed },
+			{ 'm' , ConsoleColor.Magenta },
+			{ 'M' , ConsoleColor.DarkMagenta },
+			{ 's' , ConsoleColor.Yellow },
+			{ 'S' , ConsoleColor.DarkYellow },
+			{ 'x' , ConsoleColor.Gray },
+			{ 'X' , ConsoleColor.DarkGray },
+		};
+
         public Window()
         {
         }
 
         public void Setup()
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+
             WindowSetup.SetFont((short)8, (short)8);
             
             width = Console.LargestWindowWidth;
             height = Console.LargestWindowHeight;
 			Console.SetBufferSize(width, height);
             Console.SetWindowSize(width, height);
+        }
+
+        public void DrawAtPos(char color)
+        {
+            Console.BackgroundColor = colors[color];
+            Console.Write(" ");
+
+			try
+            {
+				Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+			} catch { }
+		}
+
+        public void DrawLine((int, int) startPos, (int, int) endPos)
+        {
+
         }
     }
 }
